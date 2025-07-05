907895b86e9522a013f631957b22d37572aaae16
belunatic <abelluvyou@yahoo.com>
2025-07-05 16:05:10 -0700

    database and account function created and export out

--- app/lib/appwrite.js
+++ app/lib/appwrite.js
@@ -1,10 +1,13 @@
-import { Client, Account } from "appwrite";
+import { Client, Databases, Account, ID } from "appwrite";
 
-export const client = new Client();
-
-client
+//CONNECT TO THE APPWRITE DB
+const client = new Client()
 	.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
 	.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID); // Replace with your project ID
 
-export const account = new Account(client);
-export { ID } from "appwrite";
+const account = new Account(client);
+
+//CONNECT TO THE DB
+const databases = new Databases(client);
+
+export { account, databases, ID };
