a29712144642319fe89e6b15ee1c3e4bd533cabf
belunatic <abelluvyou@yahoo.com>
2025-07-10 16:47:38 -0700

    adding a navbar to the app/page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/style/globals.css";
+import NavBar from "./components/navBar";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<NavBar />
 				{children}
 			</body>
 		</html>
