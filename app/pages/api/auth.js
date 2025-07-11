d0728db04871bf2bd55eb5f2c8513cd508d2bb0c
belunatic <abelluvyou@yahoo.com>
2025-07-10 17:37:16 -0700

    Add authentication functions for Google login, logout, and user session management

---
+++ app/pages/api/auth.js
@@ -0,0 +1,50 @@
+// src/auth.js
+import { account, OAuthProvider } from "@/lib/appwrite";
+
+export const loginWithGoogle = async () => {
+	try {
+		const baseUrl = window.location.origin;
+		console.log(baseUrl);
+		console.log("Hello me");
+		await account.createOAuth2Token(
+			OAuthProvider.Google,
+			`${baseUrl}/admin`, // your callback URL
+			`${baseUrl}/` // failure URL
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const logoutUser = async () => {
+	try {
+		await account.deleteSession("current");
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const getUser = async () => {
+	try {
+		return await account.get();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// 3. Implement the callback handler
+export const createUserSession = async (userId, secret) => {
+	try {
+		// Create a session using the OAuth2 token
+		await account.createSession(userId, secret);
+
+		// Get the user data
+		const user = await account.get();
+		console.log("this is the user from func:  ", user);
+		// User is now authenticated!
+		return user;
+	} catch (error) {
+		console.error("Authentication failed:", error);
+		throw error;
+	}
+};
