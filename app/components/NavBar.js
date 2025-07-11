3882891e1879e18331f963219fea67b02b17ecf3
belunatic <abelluvyou@yahoo.com>
2025-07-10 21:00:20 -0700

    logo image changed to new logo

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -3,16 +3,16 @@ import Image from "next/image";
 const NavBar = () => {
 	return (
 		<div>
-			<nav className="bg-white shadow-md py-4">
+			<nav className=" shadow-md py-4 bg-nav">
 				<div className="max-w-6xl mx-auto flex items-center justify-between px-4 cursor-pointer">
 					<Image
-						src="/images/jazzHerbsLogo.png"
+						src="/images/JazzmynApothecaryLogo.png"
 						width={300}
-						height={61}
+						height={49}
 						alt="Jazz Herbs Logo"
 					/>
 					<div>
-						<button className="bg-green-700 text-white px-4 py-2 rounded cursor-pointer hover:bg-green-800 transition duration-300">
+						<button className="bg-button-primary text-white px-4 py-2 rounded cursor-pointer hover:bg-green-800 transition duration-300">
 							Login
 						</button>
 					</div>
