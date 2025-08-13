e6c93b1a75f41d840a2da799427491d798a6b860
belunatic <abelluvyou@yahoo.com>
2025-08-13 16:35:42 -0700

    fix: Correct SVG attribute casing in AddHerbButton component

--- components/AddHerbButton.js
+++ components/AddHerbButton.js
@@ -9,12 +9,12 @@ const AddHerbButton = ({ linkTo, title }) => {
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
-						stroke-width="1.5"
+						strokeWidth="1.5"
 						stroke="currentColor"
-						class="size-6">
+						className="size-6">
 						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 							d="M12 4.5v15m7.5-7.5h-15"
 						/>
 					</svg>
