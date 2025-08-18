b41eae974c4ab20ad54cc866c24bb713f8a968de
belunatic <abelluvyou@yahoo.com>
2025-08-17 18:27:50 -0700

    add fucntionality to search thru the herb data

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,16 +1,41 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 
-const SearchForm = () => {
+const SearchForm = ({ currentHerbData, setHerbData }) => {
+	const [searchTerm, setSearchTerm] = useState("");
+	const searchSubmit = () => {
+		console.log(searchTerm);
+		console.log(currentHerbData);
+		if (currentHerbData) {
+			setHerbData(
+				currentHerbData.filter((item) => {
+					console.log(item.herb_name, item.alt_name);
+					const matchesHerbName =
+						searchTerm.length === 0 ||
+						item.herb_name.toLowerCase() === searchTerm.toLowerCase();
+
+					const matchesAltName =
+						searchTerm.length === 0 ||
+						item.alt_name.toLowerCase() === searchTerm.toLowerCase();
+
+					return matchesAltName || matchesHerbName;
+				})
+			);
+		}
+	};
+
 	return (
 		<div className="flex flex-col gap-y-4 mt-6">
 			<input
-				className="block w-full p-2.5 border-text border-size border-1 border rounded-box text-text"
+				className="block w-full p-2.5 border-text border-size border-1 rounded-box text-text"
 				type="text"
 				name="search"
 				id="search"
-				onFocus={() => setConfirmMessage(false)}
+				onChange={(e) => setSearchTerm(e.target.value)}
 			/>
-			<button className="py-2 block px-1 rounded-lg cursor-pointer hover:bg-button-success/70 bg-button-success w-full">
+			<button
+				onClick={searchSubmit}
+				className="py-2 block px-1 rounded-lg cursor-pointer hover:bg-button-success/70 bg-button-success w-full">
 				Search
 			</button>
 		</div>
